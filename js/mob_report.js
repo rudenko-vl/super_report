2fbff3932fafbfc9847f20bb0776f61595a07746
Руденко  Владислав Сергійович <rvs07@i.ua>
2023-10-25 10:50:25 +0300

    burger menu

---
+++ js/mob_report.js
@@ -0,0 +1,23 @@
+const repBtn2 = document.getElementById('rep2');
+const result2 = document.getElementById('report2-result');
+
+repBtn2.addEventListener("click", () => {
+    result2.classList.remove("visually-hidden");
+    let reportCounter = localStorage.getItem("currentCounter");
+    let word;
+
+    if (reportCounter % 10 === 1 && reportCounter % 100 !== 11) {
+        word = "раз";
+    } else if (reportCounter % 10 >= 2 && reportCounter % 10 <= 4 && (reportCounter % 100 < 10 || reportCounter % 100 >= 20)) {
+        word = "раза";
+    } else {
+        word = "раз";
+    }
+    if (!reportCounter) {
+        result2.textContent = 'Вы еще не строили этот отчёт!'
+    }
+    else {
+        result2.textContent = `Вы построили отчёт ${reportCounter} ${word}`;
+    }
+
+});
