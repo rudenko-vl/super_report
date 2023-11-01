783f2227bae1d4837b035dd601929731bacc3d34
Руденко  Владислав Сергійович <rvs07@i.ua>
2023-11-01 12:55:55 +0200

    fix list styles

--- js/test.js
+++ js/test.js
@@ -8,8 +8,8 @@ const first = `
 <h2 class="test_title">${questions[0].title}</h2>
     <img class="test_img" src=${questions[0].img} alt=${questions[0].alt}>
      <ul class="list test_list ">
-        <li class="correct1">${questions[0].variants[0]}</li>
-        <li class="uncorrect1">${questions[0].variants[1]}</li>
+        <li class="test_list-item correct1">${questions[0].variants[0]}</li>
+        <li class="test_list-item uncorrect1">${questions[0].variants[1]}</li>
         <li class="uncorrect2">${questions[0].variants[2]}</li>
     </ul>
 `
@@ -19,8 +19,8 @@ const two = `
 <h2 class="test_title">${questions[1].title}</h2>
     <img class="test_img" src=${questions[1].img} alt=${questions[1].alt}>
      <ul class="list test_list ">
-        <li class="uncorrect3">${questions[1].variants[0]}</li>
-        <li class="correct2">${questions[1].variants[1]}</li>
+        <li class="test_list-item uncorrect3">${questions[1].variants[0]}</li>
+        <li class="test_list-item correct2">${questions[1].variants[1]}</li>
         <li class="uncorrect4">${questions[1].variants[2]}</li>
     </ul>
 `
@@ -28,8 +28,8 @@ const three = `
 <h2 class="test_title">${questions[2].title}</h2>
     <img class="test_img" src=${questions[2].img} alt=${questions[2].alt}>
      <ul class="list test_list ">
-        <li class="uncorrect5">${questions[2].variants[0]}</li>
-        <li class="uncorrect6">${questions[2].variants[1]}</li>
+        <li class="test_list-item uncorrect5">${questions[2].variants[0]}</li>
+        <li class="test_list-item uncorrect6">${questions[2].variants[1]}</li>
         <li class="correct3">${questions[2].variants[2]}</li>
     </ul>
 `
