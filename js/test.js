dcd2a9d963ce521fcb35dffaed58ce185b9e3d15
Руденко  Владислав Сергійович <rvs07@i.ua>
2023-11-01 13:53:41 +0200

    fix style

--- js/test.js
+++ js/test.js
@@ -8,9 +8,9 @@ const first = `
 <h2 class="test_title">${questions[0].title}</h2>
     <img class="test_img" src=${questions[0].img} alt=${questions[0].alt}>
      <ul class="list test_list ">
-        <li class="test_list-item correct1">${questions[0].variants[0]}</li>
-        <li class="test_list-item uncorrect1">${questions[0].variants[1]}</li>
-        <li class="uncorrect2">${questions[0].variants[2]}</li>
+        <li class="common-item_test test_list-item correct1">${questions[0].variants[0]}</li>
+        <li class="common-item_test test_list-item uncorrect1">${questions[0].variants[1]}</li>
+        <li class="common-item_test uncorrect2">${questions[0].variants[2]}</li>
     </ul>
 `
 box.innerHTML = first;
@@ -19,18 +19,18 @@ const two = `
 <h2 class="test_title">${questions[1].title}</h2>
     <img class="test_img" src=${questions[1].img} alt=${questions[1].alt}>
      <ul class="list test_list ">
-        <li class="test_list-item uncorrect3">${questions[1].variants[0]}</li>
-        <li class="test_list-item correct2">${questions[1].variants[1]}</li>
-        <li class="uncorrect4">${questions[1].variants[2]}</li>
+        <li class="common-item_test test_list-item uncorrect3">${questions[1].variants[0]}</li>
+        <li class="common-item_test test_list-item correct2">${questions[1].variants[1]}</li>
+        <li class="common-item_test uncorrect4">${questions[1].variants[2]}</li>
     </ul>
 `
 const three = `
 <h2 class="test_title">${questions[2].title}</h2>
     <img class="test_img" src=${questions[2].img} alt=${questions[2].alt}>
      <ul class="list test_list ">
-        <li class="test_list-item uncorrect5">${questions[2].variants[0]}</li>
-        <li class="test_list-item uncorrect6">${questions[2].variants[1]}</li>
-        <li class="correct3">${questions[2].variants[2]}</li>
+        <li class="common-item_test test_list-item uncorrect5">${questions[2].variants[0]}</li>
+        <li class="common-item_test test_list-item uncorrect6">${questions[2].variants[1]}</li>
+        <li class="common-item_test correct3">${questions[2].variants[2]}</li>
     </ul>
 `
 // ===============================
@@ -91,7 +91,7 @@ box.addEventListener('click', (event) => {
 
     target.style.color = 'green';
     setTimeout(() => {
-        box.innerHTML = `<h1>Вы успешно прошли тест!</h1>
+        box.innerHTML = `<h1 class="last-title">Вы успешно прошли тест!</h1>
         <h2 class="last_info">Действие "${option}" будет выполнено</h2>
          <a class="nav-link test-link" href="./index.html">На главную страницу</a>
         `
